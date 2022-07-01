a96c5795f5dbaa612e4a24537a047c86c0fd8ab4
Omar.Aceves <lcc.omar.aceves@gmail.com>
2022-06-30 18:07:35 -0700

    Completing min api

--- Program.cs
+++ Program.cs
@@ -5,24 +5,56 @@ builder.Services.AddDbContext<TodoDb>(opt => opt.UseInMemoryDatabase("TodoList")
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 var app = builder.Build();
+const string idPath = "/todoitems/{id}";
 
 app.MapGet("/", () => "Hello World!");
 
 app.MapGet("/todoitems", async (TodoDb db) => 
     await db.Todos.ToListAsync());
 
+// app.MapGet(idPath, async (TodoDb db) => {
+//     var result = await db.Todos.ToListAsync();
+//     if(result is null) return Results.NotFound();
+//     return Results.Ok(result);
+// });
+    
 app.MapGet("/todoitems/complete", async (TodoDb db) => 
     await db.Todos.Where(x => x.IsComplete)
         .ToListAsync());
 
-app.MapGet("/todoitems/{id}", async (int id, TodoDb db) => 
-    await db.Todos.FindAsync(id)
-        is Todo todo
-            ? Results.Ok(todo)
-            : Results.NotFound());
+app.MapGet(idPath, async (int id, TodoDb db) => 
+    await db.Todos.FindAsync(id) is Todo todo ? Results.Ok(todo) : Results.NotFound());
 
+app.MapPost(idPath, async (Todo item, TodoDb db) => {
+    db.Todos.Add(item);
+    await db.SaveChangesAsync();
 
+    return Results.Created($"/todoitems/{item.Id}", item);
+});
 
+app.MapPut(idPath, async (int id, Todo item, TodoDb db) => {
+    var result = await db.Todos.FindAsync(id);
+
+    if(result is null) return Results.NotFound();
+
+    result.IsComplete = item.IsComplete;
+    result.Name = item.Name;
+    db.Update(result);
+    await db.SaveChangesAsync();
+
+    return Results.NoContent();
+});
+
+app.MapDelete(idPath, async (int id, TodoDb db) => {
+    var result = await db.Todos.FindAsync(id);
+    
+    if(result is null) return Results.NotFound();
+
+    db.Todos.Remove(result);
+    await db.SaveChangesAsync();
+    return Results.Ok(result);
+});
+    
 app.Run();
 
 class TodoDb : DbContext
@@ -38,3 +70,5 @@ class Todo
     public string? Name { get; set; }
     public bool IsComplete { get; set; }
 }
+
+//https://docs.microsoft.com/en-us/aspnet/core/tutorials/min-web-api?view=aspnetcore-6.0&tabs=visual-studio-code
